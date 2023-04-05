e8cddd5b9811c0e13dcbed0dc3208e66adf6983a
Alexander1000000 <kovalenko1000000>
2023-04-05 21:52:13 +0300

    Initial commit

---
+++ src/main/java/com/HttpDemo.java
@@ -0,0 +1,134 @@
+package com;
+
+import com.model.*;
+import com.google.gson.*;
+import java.io.*;
+import java.net.URI;
+import java.util.*;
+
+public class HttpDemo {
+    private static final String USERS_URL = "https://jsonplaceholder.typicode.com/users";
+    private static final String POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+    private static final String RELATIVE_PATH = "src/main/resources";
+
+    public static User createNewUser() {
+        User user = new User();
+        user.setId(315);
+        user.setName("Alex");
+        user.setUsername("Al");
+        user.setEmail("Alex@gmail.com");
+        Adress adress = new Adress();
+        adress.setStreet("Garibaldi");
+        adress.setSuite("Apt. 51");
+        adress.setCity("Kharkov");
+        adress.setZipcode("61000");
+        Geo geo = new Geo();
+        geo.setLat("49°58.8486′");
+        geo.setLng("36°15.1632");
+        adress.setGeo(geo);
+        user.setAddress(adress);
+        user.setPhone("0-800-859-5263");
+        user.setWebsite("alex.com");
+        Company company = new Company();
+        company.setNameCompany("ABC");
+        company.setCatchPhrase("We work for you");
+        company.setBs("API");
+        user.setCompany(company);
+
+        return user;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //Task_1
+        //1.1
+        User user = createNewUser();
+        final User userCreated = HttpUtil.sendPOST(URI.create(USERS_URL), user);
+        System.out.println("//TASK 1\n//1.1\n" + userCreated);
+
+        //1.2
+        System.out.println("\n//1.2\nPlease enter the ID of the user whose you want to rename.");
+        int userId = new Scanner(System.in).nextInt();
+        User userForRename = HttpUtil.sendGET_User(URI.create(String.format("%s/%d", USERS_URL, userId)));
+        System.out.println("User before rename: " + userForRename);
+        userForRename.setName("Alex");
+        User userAfterRename = HttpUtil.sendPUT(URI.create(String.format("%s/%d", USERS_URL, userId)), userForRename);
+        System.out.println("User after rename: " + userAfterRename);
+
+        //1.3
+        System.out.println("\n//1.3");
+        HttpUtil.sendDELETE(URI.create(String.format("%s/%d", USERS_URL, 1)));
+
+        //1.4
+        List<User> users = HttpUtil.sendGET_ALL_USERS(URI.create(USERS_URL));
+        System.out.println("\n//1.4");
+        users.forEach(System.out::println);
+
+        //1.5
+        System.out.println("\n//1.5\nPlease enter the ID of the user you want to find.");
+        int userId1 = new Scanner(System.in).nextInt();
+        final User userById = HttpUtil.sendGET_User(URI.create(String.format("%s/%d", USERS_URL, userId1)));
+        if (userById.getId() == 0) {
+            System.out.println("This id doesn't exist.");
+        } else {
+            System.out.println(userById);
+        }
+
+        //1.6
+        System.out.println("\n//1.6\nPlease enter the username of the user you want to find.");
+        String userName = new Scanner(System.in).nextLine();
+        final User[] userByUsername = HttpUtil.sendGET_BY_USERNAME(URI.create(String.format("%s?username=%s", USERS_URL, userName)));
+        if (userByUsername.length == 0) {
+            System.out.println("This surname doesn't exist.");
+        } else {
+            StringJoiner userByUsernameString = new StringJoiner(", ");
+            for (User value : userByUsername) {
+                userByUsernameString.add(value.toString());
+            }
+            System.out.println(userByUsernameString);
+        }
+
+        //Task_2
+        System.out.println("\n//TASK2\nPlease enter the ID of the user whose comments you are interested.");
+        int userId2 = new Scanner(System.in).nextInt();
+        List<Post> allPosts = HttpUtil.sendGET_ALL_POSTS(URI.create(String.format("%s/%d/%s",
+                USERS_URL, userId2, "posts")));
+        ArrayList<Integer> arrayListPost = new ArrayList<>(allPosts.size());
+        int k = 0;
+        while (k < allPosts.size()) {
+            arrayListPost.add(k, allPosts.get(k).getId());
+            k++;
+        }
+        Integer maxIdPost = Collections.max(arrayListPost);
+        Comment[] comment = HttpUtil.sendGET_MAX_Comment_BY_ID_USER(URI.create(String.format("%s/%d/%s",
+                POSTS_URL, maxIdPost, "comments")));
+        if (comment.length == 0) {
+            System.out.println("This id doesn't exist.");
+        } else {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String json = gson.toJson(comment);
+
+            File file = new File(RELATIVE_PATH, String.format("user-%d-post-%d-comments.json", userId2, maxIdPost));
+
+            if (!file.exists()) {
+                try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+                    bufferedWriter.write(json);
+                } catch (IOException exc) {
+                    System.out.println(exc.getMessage());
+                }
+            }
+        }
+        //Task_3
+        System.out.println("\n//TASK3\nPlease enter the ID");
+        int userId3 = new Scanner(System.in).nextInt();
+        List<Todo> todos = HttpUtil.sendTodo(URI.create(String.format("%s/%d/%s", USERS_URL, userId3, "todos")));
+        if (todos.size() == 0) {
+            System.out.println("This id doesn't exist.");
+        } else {
+            for (Todo todo : todos) {
+                if (!todo.isCompleted()) {
+                    System.out.println(todo);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/model/Company.java
@@ -0,0 +1,34 @@
+package com.model;
+
+public class Company {
+    private String nameCompany;
+    private String catchPhrase;
+    private String bs;
+
+    public Company() {
+    }
+
+    public String getNameCompany() {
+        return nameCompany;
+    }
+
+    public void setNameCompany(String nameCompany) {
+        this.nameCompany = nameCompany;
+    }
+
+    public String getCatchPhrase() {
+        return catchPhrase;
+    }
+
+    public void setCatchPhrase(String catchPhrase) {
+        this.catchPhrase = catchPhrase;
+    }
+
+    public String getBs() {
+        return bs;
+    }
+
+    public void setBs(String bs) {
+        this.bs = bs;
+    }
+}
